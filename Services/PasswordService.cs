bd55b16565bafcf2aacd4f897f5802fac464a454
Dinushka20 <it23736450@my.sliit.lk>
2026-02-21 17:59:26 +0530

    SCRUM-12 Create the Backend add password hashing and JWT token service

---
+++ Services/PasswordService.cs
@@ -0,0 +1,8 @@
+namespace PATHFINDER_BACKEND.Services
+{
+    public class PasswordService
+    {
+        public string Hash(string password) => BCrypt.Net.BCrypt.HashPassword(password);
+        public bool Verify(string password, string hash) => BCrypt.Net.BCrypt.Verify(password, hash);
+    }
+}
