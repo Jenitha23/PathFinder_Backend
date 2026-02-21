d3f2c917c67c2baa9a144a9b7abe613e6ccf40fd
Dinushka20 <it23736450@my.sliit.lk>
2026-02-22 00:27:46 +0530

    Add xUnit tests for auth services and configure solution build

---
+++ PATHFINDER_BACKEND.Tests/PasswordServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using PATHFINDER_BACKEND.Services;
+using Xunit;
+
+public class PasswordServiceTests
+{
+    [Fact]
+    public void Hash_ThenVerify_WithCorrectPassword_ReturnsTrue()
+    {
+        var svc = new PasswordService();
+
+        var password = "Pass@1234";
+        var hash = svc.Hash(password);
+
+        Assert.False(string.IsNullOrWhiteSpace(hash));
+        Assert.True(svc.Verify(password, hash));
+    }
+
+    [Fact]
+    public void Verify_WithWrongPassword_ReturnsFalse()
+    {
+        var svc = new PasswordService();
+
+        var hash = svc.Hash("CorrectPass@1234");
+
+        Assert.False(svc.Verify("WrongPass@1234", hash));
+    }
+}
