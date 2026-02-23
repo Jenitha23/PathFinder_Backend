a05ae1467e8cb06746e4af59799a795e6fe4b351
Dinushka20 <it23736450@my.sliit.lk>
2026-02-23 18:48:23 +0530

    Updated the dockerfile and connecting with the Azure Database

--- Data/Db.cs
+++ Data/Db.cs
@@ -1,5 +1,27 @@
+// using Microsoft.Extensions.Configuration;
+// //using MySqlConnector;
+// using Microsoft.Data.SqlClient;
+
+// namespace PATHFINDER_BACKEND.Data
+// {
+//     public class Db
+//     {
+//         private readonly string _connStr;
+
+//         public Db(IConfiguration config)
+//         {
+//             // Try to get from environment variable first (Docker), then from appsettings
+//             _connStr = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING")
+//                       ?? config.GetConnectionString("DefaultConnection")
+//                       ?? throw new Exception("Missing ConnectionStrings:DefaultConnection or DB_CONNECTION_STRING environment variable");
+//         }
+
+//         public MySqlConnection CreateConnection() => new MySqlConnection(_connStr);
+//     }
+// }
+
 using Microsoft.Extensions.Configuration;
-using MySqlConnector;
+using Microsoft.Data.SqlClient;
 
 namespace PATHFINDER_BACKEND.Data
 {
@@ -9,10 +31,16 @@ namespace PATHFINDER_BACKEND.Data
 
         public Db(IConfiguration config)
         {
-            _connStr = config.GetConnectionString("Default")
-                      ?? throw new Exception("Missing ConnectionStrings:Default");
+            // Get connection string from env variable or appsettings.json
+            _connStr = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING")
+                      ?? config.GetConnectionString("DefaultConnection")
+                      ?? throw new Exception("Missing ConnectionStrings:DefaultConnection or DB_CONNECTION_STRING");
         }
 
-        public MySqlConnection CreateConnection() => new MySqlConnection(_connStr);
+        // Create SQL Server (Azure SQL) connection
+        public SqlConnection CreateConnection()
+        {
+            return new SqlConnection(_connStr);
+        }
     }
 }
