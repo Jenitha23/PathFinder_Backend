b9f951907666ded72b61251917392e9084144ce7
Dinushka20 <it23736450@my.sliit.lk>
2026-02-25 21:34:40 +0530

    SCRUM-28 chore(db): update Azure SQL configuration and student schema

--- Data/Db.cs
+++ Data/Db.cs
@@ -1,27 +1,5 @@
-// using Microsoft.Extensions.Configuration;
-// //using MySqlConnector;
-// using Microsoft.Data.SqlClient;
-
-// namespace PATHFINDER_BACKEND.Data
-// {
-//     public class Db
-//     {
-//         private readonly string _connStr;
-
-//         public Db(IConfiguration config)
-//         {
-//             // Try to get from environment variable first (Docker), then from appsettings
-//             _connStr = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING")
-//                       ?? config.GetConnectionString("DefaultConnection")
-//                       ?? throw new Exception("Missing ConnectionStrings:DefaultConnection or DB_CONNECTION_STRING environment variable");
-//         }
-
-//         public MySqlConnection CreateConnection() => new MySqlConnection(_connStr);
-//     }
-// }
-
 using Microsoft.Extensions.Configuration;
-using Microsoft.Data.SqlClient;
+using MySqlConnector;
 
 namespace PATHFINDER_BACKEND.Data
 {
@@ -37,10 +15,10 @@ namespace PATHFINDER_BACKEND.Data
                       ?? throw new Exception("Missing ConnectionStrings:DefaultConnection or DB_CONNECTION_STRING");
         }
 
-        // Create SQL Server (Azure SQL) connection
-        public SqlConnection CreateConnection()
+        // Create MySQL connection
+        public MySqlConnection CreateConnection()
         {
-            return new SqlConnection(_connStr);
+            return new MySqlConnection(_connStr);
         }
     }
 }
