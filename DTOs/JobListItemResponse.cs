648f253dcdaa245e05c6f4fb852034240b707f6d
it23596566 <it23596566@my.sliit.lk>
2026-04-07 13:15:43 +0530

    fix: include requirements and responsibilities in job list API response

--- DTOs/JobListItemResponse.cs
+++ DTOs/JobListItemResponse.cs
@@ -4,6 +4,8 @@ namespace PATHFINDER_BACKEND.DTOs
     {
         public int Id { get; set; }
         public string Title { get; set; } = "";
+        public string? Requirements { get; set; }      
+        public string? Responsibilities { get; set; } 
         public string CompanyName { get; set; } = "";
         public string Location { get; set; } = "";
         public string Type { get; set; } = "";
